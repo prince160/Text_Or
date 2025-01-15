ed8125c2f722da330cf3c4d01608088b75fe9a27
LeGrandDevin <stenis34@gmail.com>
2025-01-15 15:47:12 +0100

    move theme to new file

---
+++ themeDef.py
@@ -0,0 +1,30 @@
+
+
+
+dark = {
+        "bg": "#1C1C1C",
+        "fg": "#FFD700",
+        "text_bg": "#333333",
+        "text_fg": "#FFD700",
+    }
+
+light = {
+    "bg": "#FFFFFF",
+    "fg": "#000000",
+    "text_bg": "#F5F5F5",
+    "text_fg": "#000000",
+}
+
+solarizedDark = {
+    "bg": "#002b36",
+    "fg": "#073642",
+    "text_bg": "#586e75",
+    "text_fg": "#eee8d5",
+}
+
+solarizedClear = {
+    "bg": "#fdf6e3",
+    "fg": "#657b83",
+    "text_bg": "#eee8d5",
+    "text_fg": "#586e75",
+}
